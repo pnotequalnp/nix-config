e0b3c8bf397617c4cde27ea71eef7ef766e8807e
Kevin Mullins <kevin@pnotequalnp.com>
2021-06-20 13:58:29 -0700

    Add Idris dev profile for HM

---
+++ home/profiles/development/idris/default.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options.profiles.development.idris = {
+    enable = lib.mkEnableOption "Idris 2 development tooling";
+  };
+
+  config = lib.mkIf config.profiles.development.idris.enable {
+    home.packages = with pkgs; [
+      idris
+      idris2
+    ];
+  };
+}
