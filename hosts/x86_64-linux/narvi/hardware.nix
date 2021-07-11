56727e522e8a0eae106690eccdfcaf0063eced84
Kevin Mullins <kevin@pnotequalnp.com>
2021-07-11 11:36:06 -0700

    narvi: Clean up hardware config and add swapfile

--- hosts/x86_64-linux/narvi/hardware.nix
+++ hosts/x86_64-linux/narvi/hardware.nix
@@ -1,12 +1,28 @@
-{ modulesPath, ... }:
-{
+{ modulesPath, ... }: {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.loader.grub = {
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
+
+  boot = {
+    loader.grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+
+    initrd.kernelModules = [ "nvme" ];
   };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/DC0E-7E29"; fsType = "vfat"; };
-  boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/DC0E-7E29";
+    fsType = "vfat";
+  };
+
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+
+  swapDevices = [{
+    device = "/swapfile";
+    size = 4096;
+  }];
 }
