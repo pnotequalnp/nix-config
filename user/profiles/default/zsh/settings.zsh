4981a77f73641c04115f055d33817ff98e363b49
Kevin Mullins <kevin@pnotequalnp.com>
2021-07-26 21:17:40 -0700

    user: Change zsh options

--- user/profiles/default/zsh/settings.zsh
+++ user/profiles/default/zsh/settings.zsh
@@ -1,7 +1,7 @@
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history)
 stty stop undef
 stty start undef
-setopt EXTENDED_GLOB CSH_NULL_GLOB
-setopt AUTO_PUSHD PUSHD_IGNORE_DUPS
+setopt CSH_NULL_GLOB
+setopt AUTO_PUSHD PUSHD_IGNORE_DUPS PUSHD_MINUS
 unsetopt BEEP
 KEYTIMEOUT=5
