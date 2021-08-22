1bd486b3d8ef04bb8240d3402b1d95c954512f87
Kevin Mullins <kevin@pnotequalnp.com>
2021-08-22 10:50:38 -0700

    user: Add `result-doc` to global gitignore

--- user/profiles/terminal/git.nix
+++ user/profiles/terminal/git.nix
@@ -9,12 +9,16 @@
       "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) %C(white)[%G?]%C(reset) - %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
   };
 
-  ignores = [ ".direnv/" ".vscode/" ".envrc" "result/" "result" ];
+  ignores = [
+    ".direnv/"
+    ".vscode/"
+    ".envrc"
+    "result"
+    "result-doc"
+  ];
 
   extraConfig = {
-    pull = {
-      ff = "only";
-    };
+    pull.ff = "only";
     init.defaultBranch = "main";
     github.user = "pnotequalnp";
     tag.gpgSign = true;
