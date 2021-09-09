f21d94cb975ce7094ba85845f2526f750fec9158
Kevin Mullins <kevin@pnotequalnp.com>
2021-09-09 15:11:20 -0700

    system: Add nix-community binary cache

--- system/profiles/default/default.nix
+++ system/profiles/default/default.nix
@@ -20,6 +20,12 @@
 
     trustedUsers = [ "root" "@wheel" ];
 
+    binaryCaches = [ "https://nix-community.cachix.org" ];
+
+    binaryCachePublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
     registry.nixpkgs.flake = nixpkgs;
 
     extraOptions = ''
